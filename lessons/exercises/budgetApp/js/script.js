7a8b6032d07b500e38c429934d7b97f829ccae3e
Victor Dulap <anunachi1337@gmail.com>
2020-12-15 22:59:05 +0200

    added edit, still not working will fix tommorow

--- lessons/exercises/budgetApp/js/script.js
+++ lessons/exercises/budgetApp/js/script.js
@@ -74,20 +74,20 @@ class UI {
             // get current inserted item instance
             const currentItem = document.querySelector(`#list-item-${this.itemCounter}`);
             // get currentItems amount and message
-            const currentItemsAmount = currentItem.querySelector('.list-amount');
-            const currentItemsMessage = currentItem.querySelector('.list-message');
+            const currentItemAmount = currentItem.querySelector('.list-amount');
+            const currentItemMessage = currentItem.querySelector('.list-message');
 
-            // assign to event delete btn
+            // assign to event delete btn - done
             const deleteBtn = currentItem.querySelector('.delete-btn');
             deleteBtn.addEventListener('click', () => {
                // reset total
-               this.total.textContent = (parseFloat(this.total.textContent) - parseFloat(currentItemsAmount.textContent)).toFixed(2);
+               this.total.textContent = (parseFloat(this.total.textContent) - parseFloat(currentItemAmount.textContent)).toFixed(2);
 
                // reset balance
                if (this.type == 'income')
-                  this.balance.textContent = parseFloat(this.balance.textContent) - parseFloat(currentItemsAmount.textContent);
+                  this.balance.textContent = parseFloat(this.balance.textContent) - parseFloat(currentItemAmount.textContent);
                else if (this.type == 'expense')
-                  this.balance.textContent = parseFloat(this.balance.textContent) + parseFloat(currentItemsAmount.textContent);
+                  this.balance.textContent = parseFloat(this.balance.textContent) + parseFloat(currentItemAmount.textContent);
 
                   // hide edit button and reveal add button
                this.editBtn.classList.add('hide');
@@ -97,20 +97,60 @@ class UI {
                currentItem.remove();
             });
 
-            // assign to event edit btn
-            const editBtn = currentItem.querySelector('.edit-btn');
+
+
+            // get edit btn
+            var editBtn = currentItem.querySelector('.edit-btn');
+
+            // assign event to edit btn
             editBtn.addEventListener('click', () => {
                // move data to inputs
-               this.amount.value = parseFloat(currentItemsAmount.textContent);
-               this.reason.value = currentItemsMessage.textContent;
+               this.amount.value = parseFloat(currentItemAmount.textContent);
+               this.reason.value = currentItemMessage.textContent;
 
                // hide add button and reveal edit button
                this.submitBtn.classList.add('hide');
                this.editBtn.classList.remove('hide');
 
-               // hide edit button and reveal add button
-               // this.editBtn.classList.add('hide');
-               // this.submitBtn.classList.remove('hide');
+               // TODO: reset old event and add new event (current not working)
+
+               // reset old events of it
+               var editBtnClone = this.editBtn.cloneNode(true);
+               this.editBtn.parentNode.replaceChild(editBtnClone, this.editBtn);
+
+               // add new event to editBtn
+               this.editBtn.addEventListener('click', (e) => {
+                  debugger;
+                  e.preventDefault();
+
+                  // remove old value from total
+                  this.total.textContent = (parseFloat(this.total.textContent) - parseFloat(currentItemAmount.textContent)).toFixed(2);
+
+                  // remove old value from balance
+                  if (this.type == 'income')
+                     this.balance.textContent = parseFloat(this.balance.textContent) - parseFloat(currentItemAmount.textContent);
+                  else if (this.type == 'expense')
+                     this.balance.textContent = parseFloat(this.balance.textContent) + parseFloat(currentItemAmount.textContent);
+
+                  // reset list item's details
+                  currentItemAmount.textContent = this.amount.value;
+                  currentItemMessage.textContent = this.reason.value;
+
+                  // remove old value from total
+                  this.total.textContent = (parseFloat(this.total.textContent) + parseFloat(currentItemAmount.textContent)).toFixed(2);
+
+                  // remove old value from balance
+                  if (this.type == 'income')
+                     this.balance.textContent = parseFloat(this.balance.textContent) + parseFloat(currentItemAmount.textContent);
+                  else if (this.type == 'expense')
+                     this.balance.textContent = parseFloat(this.balance.textContent) - parseFloat(currentItemAmount.textContent);
+
+                  // hide edit button and reveal add button
+                  this.editBtn.classList.add('hide');
+                  this.submitBtn.classList.remove('hide');
+               });
+
+               // editBtn.removeEventListener('click')
             });
 
             // recalculate balance
@@ -143,3 +183,14 @@ submitExpenseeButton.addEventListener("click", (e) => {
    e.preventDefault();
    expenseUi.submitForm();
 });
+
+
+// //tests
+// const testBtn = document.querySelector('#test');
+// testBtn.addEventListener('click', () => {
+//    console.log('test1');
+// })
+
+// // this 2 lines removes events from testBtn
+// var testBtnClone = testBtn.cloneNode(true);
+// testBtn.parentNode.replaceChild(testBtnClone, testBtn);
